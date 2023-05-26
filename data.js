61a80b6377df3fd7f3b61da21fc26e902f9d87a7
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-05-26 21:24:31 +0100

    start building the render function

--- data.js
+++ data.js
@@ -3,36 +3,42 @@
 const fighters = {
     warrior:{
         name: "Warrior",
+        image: './images/warrior.png',
         health: 100,
         attack: 20,
         defense: 10,
     },
     goblin:{
         name: "Goblin",
+        image: './images/goblin.png',
         health: 100,
         attack: 10,
         defense: 5,
     },
     witch:{
         name: "Witch",
+        image: './images/witch.png',
         health: 100,
         attack: 15,
         defense: 10,
     },
     vampire:{
         name: "Vampire",
+        image: './images/vampire.png',
         health: 100,
         attack: 20,
         defense: 10,
     },
     devill:{
         name: "Devill",
+        image: './images/devill.png',
         health: 100,
         attack: 25,
         defense: 15,
     },
     death:{
         name: "Death",
+        image: './images/death.png',
         health: 100,
         attack: 30,
         defense: 20,
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import fighters from "./data";
+import fighters from "./data.js";
 
 class Fighter{
     constructor(obj){
@@ -7,14 +7,24 @@ class Fighter{
     }
 
     getFighterHtml(){
-        const {name, health, attack, defense} = this;
+        const {name, image, health, attack, defense} = this;
         return `
             <h3>${name}</h3>
+            <img src=${image} alt="${name}" />
             <div>Health: ${health}</div>
             <div>Attack: ${attack}</div>
             <div>Defense: ${defense}</div>
-        
-        
-    }
-
+            `;
+        }
 }
+
+const warrior = new Fighter(fighters.warrior);
+const monster = new Fighter(fighters.goblin);
+
+
+function render(){
+   document.getElementById("warrior").innerHTML = warrior.getFighterHtml();
+    document.getElementById("monster").innerHTML = monster.getFighterHtml(); 
+}
+
+render();
