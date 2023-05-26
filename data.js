cb2b564666c1d74c230e487abfa0ac93346fa492
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-05-26 20:54:01 +0100

    create data of all warriors

---
+++ data.js
@@ -0,0 +1,40 @@
+
+
+const fighters = {
+    warrior:{
+        name: "Warrior",
+        health: 100,
+        attack: 20,
+        defense: 10,
+    },
+    goblin:{
+        name: "Goblin",
+        health: 100,
+        attack: 10,
+        defense: 5,
+    },
+    witch:{
+        name: "Witch",
+        health: 100,
+        attack: 15,
+        defense: 10,
+    },
+    vampire:{
+        name: "Vampire",
+        health: 100,
+        attack: 20,
+        defense: 10,
+    },
+    devill:{
+        name: "Devill",
+        health: 100,
+        attack: 25,
+        defense: 15,
+    },
+    death:{
+        name: "Death",
+        health: 100,
+        attack: 30,
+        defense: 20,
+    },
+}
